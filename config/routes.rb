6602f1464b1e12bb6c987a70d47d2653f5dd3f22
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-27 11:21:18 +0300

    Make route for making a GET request to students -  grades

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'students', to: 'students#index'
 end
+
+#index method will always be called when user goes to /students
+# 'students#index' tells the Rails routing system
+#  that this route should be passed through 
+#  the StudentsController's index action.
